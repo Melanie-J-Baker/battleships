56cd640bd8609c90d83ce8103306e0e1ffa19549
Melanie-J-Baker <baker_mel@hotmail.com>
2023-07-22 16:35:04 +0100

    Commit before changing coordinate system to numbers

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
     },
     o: (e, n) => Object.prototype.hasOwnProperty.call(e, n),
   };
-  e.d({}, { lA: () => E, UR: () => f, n1: () => v, hM: () => p, mO: () => m });
+  e.d({}, { lA: () => p, UR: () => d, n1: () => l, hM: () => c, mO: () => h });
   const n = function () {
     var e = [],
       n = [],
@@ -44,7 +44,7 @@
           );
         })(t);
         e.push(a);
-        for (var i = 0; i < a.shipCoords.length; i++) n.push(a.shipCoords[i]);
+        for (var i = 0; i < a.shipCoords.length; i++);
       },
       receiveAttack: function (i) {
         if (!1 !== a.includes(i) || !1 !== t.includes(i))
@@ -86,7 +86,7 @@
       n < e.length;
       n++
     )
-      e[n].addEventListener("click", p.playerMove);
+      e[n].addEventListener("click", c.playerMove);
   }
   function i() {
     for (
@@ -94,7 +94,7 @@
       n < e.length;
       n++
     )
-      e[n].removeEventListener("click", p.playerMove);
+      e[n].removeEventListener("click", c.playerMove);
   }
   function r() {
     document.getElementById("info").textContent =
@@ -103,32 +103,32 @@
   function o() {
     document.getElementById("info").textContent = "You have won!";
   }
-  function u() {
+  function s() {
     document.getElementById("info").textContent =
       "Computer has sunk all your ships! You lose!";
   }
-  const s = function () {
+  const u = function () {
     var e = function (e) {
         var n =
-          f.availableMoves[Math.floor(Math.random() * f.availableMoves.length)];
+          d.availableMoves[Math.floor(Math.random() * d.availableMoves.length)];
         e.receiveAttack(n);
-        var t = f.availableMoves.indexOf(n);
-        t > -1 && f.availableMoves.splice(t, 1);
+        var t = d.availableMoves.indexOf(n);
+        t > -1 && d.availableMoves.splice(t, 1);
       },
       n = function (e) {
-        p.attack(v, e.target.id.slice(1)),
-          t(v),
+        c.attack(l, e.target.id.slice(1)),
+          t(l),
           i(),
-          !0 === v.allSunk()
+          !0 === l.allSunk()
             ? o()
-            : !0 === m.allSunk()
-            ? u()
+            : !0 === h.allSunk()
+            ? s()
             : ((document.getElementById("info").textContent =
                 "Computer is taking their turn."),
-              setTimeout(s, 1e3));
+              setTimeout(u, 1e3));
       },
-      s = function () {
-        e(m),
+      u = function () {
+        e(h),
           (function (e) {
             for (
               var n = document.getElementsByClassName("square pSquare"), t = 0;
@@ -139,24 +139,24 @@
                 ? (n[t].className = "square pSquare hit")
                 : !0 === e.misses.includes(n[t].id.slice(1)) &&
                   (n[t].className = "square pSquare miss");
-          })(m),
-          !0 === v.allSunk()
+          })(h),
+          !0 === l.allSunk()
             ? (o(), i())
-            : !0 === m.allSunk()
-            ? (u(), i())
+            : !0 === h.allSunk()
+            ? (s(), i())
             : (r(), a());
       };
     return {
       attack: function (e, a) {
-        if (p.availableMoves.includes(a)) {
+        if (c.availableMoves.includes(a)) {
           e.receiveAttack(a);
-          var r = p.availableMoves.indexOf(a);
-          r > -1 && p.availableMoves.splice(r, 1);
+          var r = c.availableMoves.indexOf(a);
+          r > -1 && c.availableMoves.splice(r, 1);
         } else
           (document.getElementById("info").textContent =
             "That square has already been attacked!"),
             i(),
-            t(v),
+            t(l),
             n();
       },
       randomAttack: e,
@@ -263,188 +263,34 @@
         "J10",
       ],
       playerMove: n,
-      computerMove: s,
+      computerMove: u,
     };
   };
-  var c = [
-      "A1",
-      "A2",
-      "A3",
-      "A4",
-      "A5",
-      "A6",
-      "A7",
-      "A8",
-      "A9",
-      "A10",
-      "B1",
-      "B2",
-      "B3",
-      "B4",
-      "B5",
-      "B6",
-      "B7",
-      "B8",
-      "B9",
-      "B10",
-      "C1",
-      "C2",
-      "C3",
-      "C4",
-      "C5",
-      "C6",
-      "C7",
-      "C8",
-      "C9",
-      "C10",
-      "D1",
-      "D2",
-      "D3",
-      "D4",
-      "D5",
-      "D6",
-      "D7",
-      "D8",
-      "D9",
-      "D10",
-      "E1",
-      "E2",
-      "E3",
-      "E4",
-      "E5",
-      "E6",
-      "E7",
-      "E8",
-      "E9",
-      "E10",
-      "F1",
-      "F2",
-      "F3",
-      "F4",
-      "F5",
-      "F6",
-      "F7",
-      "F8",
-      "F9",
-      "F10",
-      "G1",
-      "G2",
-      "G3",
-      "G4",
-      "G5",
-      "G6",
-      "G7",
-      "G8",
-      "G9",
-      "G10",
-      "H1",
-      "H2",
-      "H3",
-      "H4",
-      "H5",
-      "H6",
-      "H7",
-      "H8",
-      "H9",
-      "H10",
-      "I1",
-      "I2",
-      "I3",
-      "I4",
-      "I5",
-      "I6",
-      "I7",
-      "I8",
-      "I9",
-      "I10",
-      "J1",
-      "J2",
-      "J3",
-      "J4",
-      "J5",
-      "J6",
-      "J7",
-      "J8",
-      "J9",
-      "J10",
-    ],
-    l = function (e, n) {
-      var t,
-        a,
-        i = parseInt(e / 10),
-        r = n[i].findIndex(function (n) {
-          return n === e;
-        }),
-        o = n[i][r + 1],
-        u = n[i][r - 1],
-        s = null === (t = n[i - 1]) || void 0 === t ? void 0 : t[r];
-      return {
-        right: o,
-        left: u,
-        bottom: null === (a = n[i + 1]) || void 0 === a ? void 0 : a[r],
-        top: s,
-      };
-    },
-    d = function () {
-      return Math.floor(100 * Math.random());
-    },
-    h = [
-      ["", "", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-      ["", ""],
-      ["", ""],
-      ["", ""],
-      ["", ""],
-    ],
-    p = s(),
-    v = n(),
-    f = s(),
-    m = n(),
-    C = (function () {
-      for (var e = [], n = 0, t = 0; t < 10; t++) {
-        e.push([]);
-        for (var a = 0; a < 10 && n < 100; a++) e[t].push(n), n++;
-      }
-      return e;
-    })();
-  function E() {
-    for (var e = 0; e < h.length; e++)
-      for (var n = 0; n < h[e].length; n++) i();
-    function i() {
-      var e,
-        n = d(),
-        t = l(n, C);
-      if (m.occupied.includes(c[n])) i();
-      else
-        for (var a in t) {
-          if (!m.occupied.includes(a)) return (e = c[n]), console.log(e), e;
-          i();
-        }
-    }
-    m.newShip(["G1", "G2", "G3", "G4", "G5"]),
-      m.newShip(["B2", "C2", "D2", "E2"]),
-      m.newShip(["I4", "I5", "I6", "I7"]),
-      m.newShip(["A5", "A6", "A7"]),
-      m.newShip(["D5", "D6", "D7"]),
-      m.newShip(["H9", "I9", "J9"]),
-      m.newShip(["J1", "J2"]),
-      m.newShip(["C9", "D9"]),
-      m.newShip(["A10", "B10"]),
-      m.newShip(["E10", "F10"]),
-      v.newShip(["A6", "A7", "A8", "A9", "A10"]),
-      v.newShip(["H2", "H3", "H4", "H5"]),
-      v.newShip(["F9", "G9", "H9", "I9"]),
-      v.newShip(["C2", "D2", "E2"]),
-      v.newShip(["J5", "J6", "J7"]),
-      v.newShip(["D7", "D8", "D9"]),
-      v.newShip(["A1", "A2"]),
-      v.newShip(["J1", "J2"]),
-      v.newShip(["E5", "F5"]),
-      v.newShip(["B4", "C4"]),
+  var c = u(),
+    l = n(),
+    d = u(),
+    h = n();
+  function p() {
+    h.newShip(["G1", "G2", "G3", "G4", "G5"]),
+      h.newShip(["B2", "C2", "D2", "E2"]),
+      h.newShip(["I4", "I5", "I6", "I7"]),
+      h.newShip(["A5", "A6", "A7"]),
+      h.newShip(["D5", "D6", "D7"]),
+      h.newShip(["H9", "I9", "J9"]),
+      h.newShip(["J1", "J2"]),
+      h.newShip(["C9", "D9"]),
+      h.newShip(["A10", "B10"]),
+      h.newShip(["E10", "F10"]),
+      l.newShip(["A6", "A7", "A8", "A9", "A10"]),
+      l.newShip(["H2", "H3", "H4", "H5"]),
+      l.newShip(["F9", "G9", "H9", "I9"]),
+      l.newShip(["C2", "D2", "E2"]),
+      l.newShip(["J5", "J6", "J7"]),
+      l.newShip(["D7", "D8", "D9"]),
+      l.newShip(["A1", "A2"]),
+      l.newShip(["J1", "J2"]),
+      l.newShip(["E5", "F5"]),
+      l.newShip(["B4", "C4"]),
       (function (e) {
         for (
           var n = document.getElementById("playerGrid").children, t = 0;
@@ -455,12 +301,12 @@
           e.occupied.includes(a) &&
             (n[t].className = "square pSquare occupied");
         }
-      })(m),
-      t(v),
+      })(h),
+      t(l),
       r(),
       a();
   }
-  document.getElementById("start").addEventListener("click", E),
+  document.getElementById("start").addEventListener("click", p),
     (function (e) {
       for (
         var n = document.getElementById("playerGrid"),
@@ -478,5 +324,5 @@
           n.appendChild(i),
           t.appendChild(r);
       }
-    })(p);
+    })(c);
 })();
